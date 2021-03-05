fc3282f5cbb3888b4f0f2a7d59cdf38f49aeb139
AdriCQ <adrian.capote95@zoho.com>
2021-03-05 02:25:38 -0500

    Handle errors and status

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
+import { AxiosError } from 'axios';
 import { IServices, IDictionary } from 'src/types';
+import { UserStore } from 'src/store/modules';
 
 export class ServiceProvider
 {
@@ -12,9 +14,43 @@ export class ServiceProvider
         // console.log(_response.data);
         if (_resp.STATUS)
           resolve(_callback(_resp.DATA));
-        else
+        // else
+        // {
+        //   const errors: string[] = [];
+        //   if (Array.isArray(_resp.ERRORS))
+        //   {
+        //     for (const error of _resp.ERRORS as unknown[])
+        //     {
+        //       errors.push(String(error));
+        //     }
+        //   }
+        //   else
+        //   {
+        //     for (const error in _resp.ERRORS as IDictionary<string | string[]>)
+        //     {
+        //       errors.push(String(_resp.ERRORS[error]));
+        //     }
+        //   }
+        //   reject(errors);
+        // }
+      }).catch((_error: AxiosError<IServices.iApiResponse<unknown>>) =>
+      {
+        // console.log(_error.code);
+        if (_error.response && _error.response.status)
+        {
+          // Manual Handle errors
+          switch (_error.response.status)
+          {
+            case 401:
+              UserStore.logout();
+              break;
+
+          }
+        }
+        if (_error.response?.data)
         {
           const errors: string[] = [];
+          const _resp = _error.response?.data;
           if (Array.isArray(_resp.ERRORS))
           {
             for (const error of _resp.ERRORS as unknown[])
@@ -31,12 +67,10 @@ export class ServiceProvider
           }
           reject(errors);
         }
-      }).catch(error =>
-      {
-        if (Array.isArray(error))
-          reject(error);
         else
-          reject([error])
+        {
+          reject([_error])
+        }
       })
     })
   }
--- src/store/modules/App.ts
+++ src/store/modules/App.ts
@@ -1,37 +1,34 @@
 import { VuexModule, Module, Mutation } from 'vuex-class-modules';
 import { storeInstance } from 'src/store';
-import { UserStore } from 'src/store/modules';
+// import { UserStore } from 'src/store/modules';
 import { Notify } from 'quasar'
-
 @Module({ generateMutationSetters: true })
 class AppModule extends VuexModule
 {
   errors: string[] = [];
-  version = 0.01;
+  version = 0.1;
 
   @Mutation
   handleErrors (_errors: string[])
   {
     this.errors = _errors;
-    console.log('Handle Errors', this.errors);
-    this.errors.forEach(_error =>
+    // console.log('Handle Errors', this.errors);
+    this.errors.forEach((_error: string) =>
     {
-      if (String(_error).includes('401'))
+      let icon = 'mdi-alert';
+      // Network Error
+      console.log(String(_error).toLocaleLowerCase());
+      if (String(_error).toLocaleLowerCase().includes('network error'))
       {
-        UserStore.logout();
-      } else
-      {
-        let icon = 'mdi-alert';
-        // Network Error
-        if (String(_error).toLocaleLowerCase().includes('network error'))
-          icon = 'mdi-signal-off';
-        // else if (String(_error).toLocaleLowerCase().includes('network error'))
-        Notify.create({
-          message: String(_error),
-          icon: icon,
-          color: 'negative',
-        })
+        _error = 'Error en la Red';
+        icon = 'mdi-signal-off';
       }
+      Notify.create({
+        message: String(_error),
+        icon: icon,
+        color: 'negative',
+      })
+
     })
   }
 }
