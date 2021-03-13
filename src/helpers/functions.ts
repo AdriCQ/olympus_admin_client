bc2d140be0bbc6987d16a684dcf1eb49dc3404c5
AdriCQ <adrian.capote95@zoho.com>
2021-03-12 22:17:08 -0500

    Added products images

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,33 +1,34 @@
-import { IImage } from 'src/types';
-import { SERVER_URL } from 'src/helpers';
-import { ServiceProvider, ShopOrderService } from 'src/services';
-
+import { IImage, IOlympusService } from 'src/types';
+import { PALREY_APP_TOKEN, SERVER_URL } from 'src/helpers';
+import { ServiceProvider, ShopOrderService, OlympusService } from 'src/services';
 import { HapticsNotificationType, Plugins } from '@capacitor/core';
+import { AppStore } from 'src/store/modules';
 const { LocalNotifications, Haptics } = Plugins;
 /**
  * @class Function helper 
  */
 export class FunctionHelper
 {
+
   /**
    * Handles image
    * @param _image 
    * @param [_size] 
    * @returns  
    */
-  static handleImage (_image: IImage, _size: 'sm' | 'md' | 'lg' = 'sm')
+  static handleImage (_image: IImage, _size: 'sm' | 'md' | 'lg' = 'md'): string
   {
     const localImages = [
-      { id: 1, paths: { sm: 'img/sm_1.jpg', md: 'img/md_1.jpg', lg: 'img/lg_1.jpg' } },
       { id: 2, paths: { sm: 'img/products/sm_2.jpg', md: 'img/products/md_2.jpg', lg: 'img/products/lg_2.jpg' } },
       { id: 3, paths: { sm: 'img/products/sm_3.jpg', md: 'img/products/md_3.jpg', lg: 'img/products/lg_3.jpg' } },
       { id: 4, paths: { sm: 'img/products/sm_4.jpg', md: 'img/products/md_4.jpg', lg: 'img/products/lg_4.jpg' } },
+      { id: 5, paths: { sm: 'img/products/sm_5.jpg', md: 'img/products/md_5.jpg', lg: 'img/products/lg_5.jpg' } },
     ];
     const search = localImages.find(_findImage =>
     {
       return _image.id === _findImage.id;
     })
-    return search ? search.paths[_size] : SERVER_URL + _image.paths[_size];
+    return search ? search.paths[_size] : SERVER_URL + String(_image.paths[_size]);
   }
 
   /**
@@ -58,6 +59,14 @@ export class FunctionHelper
           }).catch((_err) => console.log(_err));
         }
       })).catch(_err => console.log(_err));
+
+      /**
+       * Get Daily Visits
+       */
+      void ServiceProvider.callableService(OlympusService.getAppInfo(PALREY_APP_TOKEN), (_resp: IOlympusService.Application) =>
+      {
+        AppStore.palrey_daily_visits = _resp.daily_visits;
+      }).catch((_err) => console.log('AppInfoError', _err))
     }, 3 * 60 * 1000)
   }
 }
